6fe6e636ccea44df0f6f545b2559b18105c2127a
TheintTheintSH <theinttheintsoehtet.edka@gmail.com>
2024-10-04 16:43:03 +0630

    add id to contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,7 +5,7 @@ import { getImageUrl } from '../../utils'
 
 export const Contact = () => {
   return (
-    <footer className={styles.container}>
+    <footer id='contact' className={styles.container}>
         <div className={styles.text}>
             <h2>Contact</h2>
             <p>Feel free to reach out!</p>
